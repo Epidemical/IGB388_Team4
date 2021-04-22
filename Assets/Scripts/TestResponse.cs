ab1bcd2ca431ddbc6d7a9e6dc48d5b01b3104428
Laura <51978253+Epidemical@users.noreply.github.com>
2021-04-22 18:52:15 +1000

    bad button implementation

---
+++ Assets/Scripts/TestResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestResponse : MonoBehaviour
+{
+    public void ButtonTest()
+    {
+        print("Button Pressed");
+    }
+}
